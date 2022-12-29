bd3fab22136521eb6d94327328ef3bd323998e48
markwen0329 <114090782+markwen0329@users.noreply.github.com>
2022-12-29 19:40:33 +0800

    Update 453.cpp

--- 453.cpp
+++ 453.cpp
@@ -1,1 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num;
+printf("plese enter a number");
+scanf("%d", num);
+}
+if num
+ax2+bx+c=0    (x-a1)(x-a2) = 0  x^2 + (a1 + a2) x + a1*a2 = 0 
+
+=>   b/a = a1 + a2  , c/a = a1 * a2 
 
