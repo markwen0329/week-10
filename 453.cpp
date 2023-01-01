a3a072e1787a97ff3c7b79b98efbef5ec4ae8a83
markwen0329 <114090782+markwen0329@users.noreply.github.com>
2023-01-02 02:03:15 +0800

    Update 453.cpp

--- 453.cpp
+++ 453.cpp
@@ -5,7 +5,15 @@ int num;
 printf("plese enter a number");
 scanf("%d", num);
 }
-if num
+ifmain () {
+if num>=0 and num==int(num**0.5)*int(num**0.5):
+    printf("Yes")
+  elif num<0:
+    printf("No")
+  else:
+    printf("No")
+}
+return = 0
 ax2+bx+c=0    (x-a1)(x-a2) = 0  x^2 + (a1 + a2) x + a1*a2 = 0 
 
 =>   b/a = a1 + a2  , c/a = a1 * a2 
